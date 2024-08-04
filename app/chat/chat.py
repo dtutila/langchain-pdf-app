b34fecaba89852d14c87c317017eed342464feba
dtutila <daniel@dtutila.com>
2024-08-04 15:02:18 -0600

    adding streaming

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,16 +1,15 @@
 from app.chat.models import ChatArgs
 from app.chat.vector_stores.pinecode import build_retriever
-from langchain.chains import ConversationalRetrievalChain
 from app.chat.llms.chatopenai import build_llm
 from app.chat.memories.sql_memory import build_memory
-
+from app.chat.chains.retrieval import StreamingConversationalStreamingChain
 
 
 def build_chat(chat_args: ChatArgs):
     retriever = build_retriever(chat_args)
     llm = build_llm(chat_args)
     memory = build_memory(chat_args)
-    return ConversationalRetrievalChain.from_llm(
+    return StreamingConversationalStreamingChain.from_llm(
         retriever=retriever,
         llm=llm,
         memory=memory
